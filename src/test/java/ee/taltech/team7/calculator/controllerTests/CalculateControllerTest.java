ab6c8564bee23ade8ac77e4a45d1d5d02f527add
tanveer <mdtanveerh98@gmail.com>
2019-10-10 17:43:21 +0300

    Added todo's

---
+++ src/test/java/ee/taltech/team7/calculator/controllerTests/CalculateControllerTest.java
@@ -0,0 +1,10 @@
+package ee.taltech.team7.calculator.controllerTests;
+
+public class CalculateControllerTest {
+
+    //TODO: write 3 unit test for the controller itself
+    // 1. null input list gives 0 as answer
+    // 2. INT.MIN + some other value throws exception
+    // 3. INT.MAX + some other value throws exception
+    // 4. Same values return zero
+}
