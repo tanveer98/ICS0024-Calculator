3851e17a0f3c62ea4bbb780e0908ad898901fdde
tanveer <mdtanveerh98@gmail.com>
2019-10-20 12:02:27 +0300

    Minor change, OverflowedLongException will return HttpStatus

--- src/main/java/ee/taltech/team7/calculator/exceptions/OverflowedLongException.java
+++ src/main/java/ee/taltech/team7/calculator/exceptions/OverflowedLongException.java
@@ -1,5 +1,9 @@
 package ee.taltech.team7.calculator.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "The return value has overflowed!")
 public class OverflowedLongException extends Exception {
     public OverflowedLongException(String msg) {
         super(msg);
